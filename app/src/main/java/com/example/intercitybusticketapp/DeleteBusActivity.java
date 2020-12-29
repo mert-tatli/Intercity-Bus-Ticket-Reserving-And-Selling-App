2d114eb8652c04cdc02a8f96ef6b295604844478
bilgee23 <73483481+bilgee23@users.noreply.github.com>
2020-12-30 02:31:36 +0300

    database islemleri eklendi

--- app/src/main/java/com/example/intercitybusticketapp/DeleteBusActivity.java
+++ app/src/main/java/com/example/intercitybusticketapp/DeleteBusActivity.java
@@ -3,12 +3,28 @@ package com.example.intercitybusticketapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DeleteBusActivity extends AppCompatActivity {
-
+    EditText deletebusId;
+    Button deletebusButton;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_bus);
+        deletebusId = findViewById(R.id.inputBusPlateDelete);
+        deletebusButton =findViewById(R.id.deleteBus);
+        mDatabase= FirebaseDatabase.getInstance().getReference();
+
+    }
+    public void deleteBus(View view){
+        String deleteBusIds = deletebusId.getText().toString();
+        mDatabase.child("Buses").child(deleteBusIds).setValue(null);
     }
 }
